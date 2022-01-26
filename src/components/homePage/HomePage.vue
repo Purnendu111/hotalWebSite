c5d986215f96437a0db51843a3f82ac9dabda912
Purnendu Pandey <rishupandey852@gmail.com>
2022-01-26 17:59:55 +0530

    ..

--- src/components/homePage/HomePage.vue
+++ src/components/homePage/HomePage.vue
@@ -1,105 +1,33 @@
 <template>
-	<div style="padding: 0; height: 100%">
-		<b-carousel
-			id="carousel-1"
-			fade
-			v-model="slide"
-			:interval="100000"
-			controls
-			indicators
-			background="#ababab"
-			style="text-shadow: 1px 1px 2px #333; height: 100%"
-			@sliding-start="onSlideStart"
-			@sliding-end="onSlideEnd"
-		>
-			<!-- Text slides with image -->
-			<b-carousel-slide
-				class="cstmCarousel"
-				caption="Decorate & Celebrate Your Special Day with Us"
-				height="100%"
-			>
-				<template v-slot:img>
-					<img
-						class="d-block w-100"
-						:src="getImgUrl('front.jpg')"
-						style="width: 100% !important; height: 566px !important"
-						alt="image slot"
-					/>
-				</template>
-			</b-carousel-slide>
-
-			<!-- Slides with custom text -->
-			<b-carousel-slide>
-				<template v-slot:img>
-					<img
-						class="d-block w-100"
-						:src="getImgUrl('1.jpg')"
-						style="width: 100% !important; height: 566px !important"
-						alt="image slot"
-					/>
-				</template>
-				<h1>Engagement</h1>
-			</b-carousel-slide>
-
-			<!-- Slides with image only -->
-			<b-carousel-slide>
-				<template v-slot:img>
-					<img
-						class="d-block w-100"
-						:src="getImgUrl('office party.jpg')"
-						style="width: 100% !important; height: 566px !important"
-						alt="image slot"
-					/>
-				</template>
-				<h1>Office party</h1>
-			</b-carousel-slide>
-
-			<!-- Slides with img slot -->
-			<!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
-			<b-carousel-slide>
-				<template v-slot:img>
-					<img
-						class="d-block w-100"
-						style="width: 100% !important; height: 566px !important"
-						:src="getImgUrl('FlimingLocation.jpg')"
-						alt="image slot"
-					/>
-				</template>
-				<h1>Filming Location</h1>
-			</b-carousel-slide>
-			<b-carousel-slide>
-				<template v-slot:img>
-					<img
-						class="d-block w-100"
-						style="width: 100% !important; height: 566px !important"
-						:src="getImgUrl('wedding.jpeg')"
-						alt="image slot"
-					/>
-				</template>
-				<h1>Wedding Services</h1>
-			</b-carousel-slide>
-		</b-carousel>
+	<div style="padding: 0">
+		<b-embed
+			type="iframe"
+			aspect="4by3"
+			src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0"
+			allowfullscreen
+		></b-embed>
 		<div style="">
-			<VueCtkDateTimePicker
-				id="myDatePicker"
-				class="myDatePicker"
-				:disabled-dates="disabledDates"
-				:min-date="minDate"
-				v-model="dateTime"
-			/>
+			<HotelDatePicker
+				:disabledDates="['2022-01-28', '2022-02-11', '2018-02-21']"
+			>
+			</HotelDatePicker>
 		</div>
 	</div>
 </template>
 
 <script>
+import HotelDatePicker from "vue-hotel-datepicker";
+import "vue-hotel-datepicker/dist/vueHotelDatepicker.css";
 export default {
+	components: {
+		HotelDatePicker,
+	},
 	data() {
 		return {
 			slide: 0,
 			sliding: null,
 			dateTime: null,
-			disabledDates: ["2022-01-25", "2022-01-27"],
-			disabledHours: ["2022-01-25", "2022-01-27"],
+			disabledDates: ["2022-01-28", "2022-02-11", "2018-02-21"],
 			minDate: "",
 		};
 	},
