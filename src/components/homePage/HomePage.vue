5aea60cb9ab8275b59e9c9957bad713392ce1955
Purnendu Pandey <rishupandey852@gmail.com>
2022-01-26 19:06:01 +0530

    1

--- src/components/homePage/HomePage.vue
+++ src/components/homePage/HomePage.vue
@@ -1,33 +1,69 @@
 <template>
-	<div style="padding: 0">
+	<div style="padding: 0; height: 100%">
 		<b-embed
 			type="iframe"
-			aspect="4by3"
+			aspect="16by9"
 			src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0"
 			allowfullscreen
 		></b-embed>
 		<div style="">
-			<HotelDatePicker
-				:disabledDates="['2022-01-28', '2022-02-11', '2018-02-21']"
-			>
-			</HotelDatePicker>
+			<b-row>
+				<b-col>
+					<VueCtkDateTimePicker
+						id="myDatePicker"
+						class="myDatePicker"
+						:disabled-dates="disabledDates"
+						:min-date="minDate"
+						:range="true"
+						v-model="dateTime"
+					/>
+				</b-col>
+				<b-col>
+					<b-form-select
+						id="adults"
+						name="adults"
+						class="paddingLeft10"
+						style="width: 100% !important"
+						menu-class="w-100"
+						v-model="adults"
+					>
+						<b-form-select-option :value="null"
+							>How many Adults</b-form-select-option
+						>
+						<b-form-select-option :value="10"
+							>1</b-form-select-option
+						>
+						<b-form-select-option :value="25"
+							>2</b-form-select-option
+						>
+						<b-form-select-option :value="50"
+							>3</b-form-select-option
+						>
+						<b-form-select-option :value="100"
+							>4</b-form-select-option
+						>
+						<b-form-select-option :value="100"
+							>5</b-form-select-option
+						>
+						<b-form-select-option :value="100"
+							>6</b-form-select-option
+						>
+					</b-form-select>
+				</b-col>
+			</b-row>
 		</div>
 	</div>
 </template>
 
 <script>
-import HotelDatePicker from "vue-hotel-datepicker";
-import "vue-hotel-datepicker/dist/vueHotelDatepicker.css";
 export default {
-	components: {
-		HotelDatePicker,
-	},
 	data() {
 		return {
 			slide: 0,
 			sliding: null,
 			dateTime: null,
-			disabledDates: ["2022-01-28", "2022-02-11", "2018-02-21"],
+			disabledDates: ["2022-01-25", "2022-01-27"],
+			disabledHours: ["2022-01-25", "2022-01-27"],
 			minDate: "",
 		};
 	},
