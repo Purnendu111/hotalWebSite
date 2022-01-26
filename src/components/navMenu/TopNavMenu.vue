be27b7ea200b65fa863b218a2831df0908ee4075
Anindita <aninditaroychowdhury68@gmail.com>
2022-01-26 11:27:05 +0530

    added new feature

--- src/components/navMenu/TopNavMenu.vue
+++ src/components/navMenu/TopNavMenu.vue
@@ -1,6 +1,6 @@
 <template>
     <div  style="background: #003724">
-        <b-navbar toggleable="lg" style="margin-left: 60px !important; height: 80px">
+        <b-navbar toggleable="lg" style="margin-left: 60px !important; height: 110px">
             <b-navbar-brand href="/"
                 >
                 <span class="logoTxt"> MalharGreens </span>
@@ -10,11 +10,12 @@
 
             <b-collapse id="nav-collapse" is-nav>
                 <b-navbar-nav class="NavUL">
-                    <b-nav-item class="navMenuAText marginL70" href="">Home</b-nav-item>
+                    <b-nav-item class="navMenuAText marginL70" href="/">Home</b-nav-item>
                     <b-nav-item class="navMenuAText marginL70" href="">Rooms</b-nav-item>
-                    <b-nav-item class="navMenuAText marginL70" href="">Gallery</b-nav-item>
-                    <b-nav-item class="navMenuAText marginL70" href="">About Us</b-nav-item>
-                    <b-nav-item class="navMenuAText marginL70" href="">Contact Us</b-nav-item>
+                    <b-nav-item class="navMenuAText marginL70" href="/gallary">Gallery</b-nav-item>
+                    <b-nav-item class="navMenuAText marginL70" href="">Associate Services</b-nav-item>
+                    <b-nav-item class="navMenuAText marginL70" href="/about_us">About Us</b-nav-item>
+                    <b-nav-item class="navMenuAText marginL70" href="/contact">Contact Us</b-nav-item>
                 </b-navbar-nav>
             </b-collapse>
         </b-navbar>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,9 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../components/homePage/HomePage.vue';
-
+import About_Us from '../components/About_Us/us.vue'; 
+import Contact_Us from '../components/About_Us/contact.vue';
+import Gallary from '../components/About_Us/gallary.vue';
 Vue.use(VueRouter);
 
 const routes = [
@@ -9,6 +11,21 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home,
+  },
+  {
+    path: '/about_us',
+    name: 'About_Us',
+    component: About_Us,
+  },
+  {
+    path: '/contact',
+    name: 'Contact_Us',
+    component: Contact_Us,
+  },
+  {
+    path: '/gallary',
+    name: 'Gallary',
+    component: Gallary,
   },
 ];
 
