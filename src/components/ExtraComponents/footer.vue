0ca75dd90412fd9685c2266756f5fbc6f7feb267
Anindita <aninditaroychowdhury68@gmail.com>
2022-03-02 12:59:37 +0530

    footer

--- src/components/ExtraComponents/footer.vue
+++ src/components/ExtraComponents/footer.vue
@@ -1,7 +1,167 @@
 <template>
 	<div class="footerDiv" style="">
 		<hr style="width:100%;height:2px;border-width:0;color:gray;background-color:gray">
-		<b-row>
+		<!-- Footer -->
+<footer class="page-footer font-small unique-color-dark">
+
+  <div style="background-color: #8aa695;">
+    <div class="container">
+
+      <!-- Grid row-->
+      <div class="row py-4 d-flex align-items-center">
+
+        <!-- Grid column -->
+        <div class="col-md-6 col-lg-5 text-center text-md-left mb-4 mb-md-0">
+        <h6 class="mb-0" style="color:white;">Get connected with us on social networks!</h6>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-6 col-lg-7 text-center text-md-right">
+
+          <!-- Facebook -->
+          <a class="fb-ic">
+            <i class="fab fa-facebook-f white-text mr-4"> </i>
+          </a>
+          <!-- Twitter -->
+          <a class="tw-ic">
+            <i class="fab fa-twitter white-text mr-4"> </i>
+          </a>
+          <!-- Google +-->
+          <a class="gplus-ic">
+            <i class="fab fa-google white-text mr-4"> </i>
+          </a>
+          <!--Instagram-->
+          <a class="ins-ic">
+            <i class="fab fa-instagram white-text"> </i>
+          </a>
+
+        </div>
+        <!-- Grid column -->
+	
+      </div>
+      <!-- Grid row-->
+
+    </div>
+  </div>
+
+  <!-- Footer Links -->
+  
+
+        <!-- Content -->
+        <h6 class="text-uppercase font-weight-bold"></h6>
+        <hr>
+      
+
+  
+  <!-- Footer Links -->
+	<div class="container">
+	<h6 class="font-weight-bold">Here is some of our Works!</h6>
+    <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 240px;">
+    <!--Grid row-->
+    <div class="row">
+
+      <!--Grid column-->
+    <div class="col-lg-2 col-md-12 mb-4">
+
+        <!--Image-->
+        <div class="view overlay z-depth-1-half">
+          <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(73).webp" class="img-fluid"
+            alt="">
+          <a href="">
+            <div class="mask rgba-white-light"></div>
+          </a>
+        </div>
+
+    </div>
+      <!--Grid column-->
+
+      <!--Grid column-->
+    <div class="col-lg-2 col-md-6 mb-4">
+
+        <!--Image-->
+        <div class="view overlay z-depth-1-half">
+          <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(78).webp" class="img-fluid"
+            alt="">
+          <a href="">
+            <div class="mask rgba-white-light"></div>
+          </a>
+        </div>
+
+    </div>
+      <!--Grid column-->
+
+      <!--Grid column-->
+    <div class="col-lg-2 col-md-6 mb-4">
+
+        <!--Image-->
+        <div class="view overlay z-depth-1-half">
+          <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(79).webp" class="img-fluid"
+            alt="">
+          <a href="">
+            <div class="mask rgba-white-light"></div>
+          </a>
+        </div>
+
+    </div>
+      <!--Grid column-->
+
+      <!--Grid column-->
+    <div class="col-lg-2 col-md-12 mb-4">
+
+        <!--Image-->
+        <div class="view overlay z-depth-1-half">
+          <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(81).webp" class="img-fluid"
+            alt="">
+          <a href="">
+            <div class="mask rgba-white-light"></div>
+          </a>
+        </div>
+
+    </div>
+      <!--Grid column-->
+
+      <!--Grid column-->
+    <div class="col-lg-2 col-md-6 mb-4">
+
+        <!--Image-->
+        <div class="view overlay z-depth-1-half">
+          <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(82).webp" class="img-fluid"
+            alt="">
+          <a href="">
+            <div class="mask rgba-white-light"></div>
+          </a>
+        </div>
+
+    </div>
+      <!--Grid column-->
+
+      <!--Grid column-->
+    <div class="col-lg-2 col-md-6 mb-4">
+
+        <!--Image-->
+        <div class="view overlay z-depth-1-half">
+          <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(84).webp" class="img-fluid"
+            alt="">
+          <a href="">
+            <div class="mask rgba-white-light"></div>
+          </a>
+        </div>
+		
+    </div>
+      <!--Grid column-->
+    
+    </div>
+    <!--Grid row-->
+
+  </div>
+  <!-- Footer Elements -->
+
+<h6 class="font-weight-bold" style="text-align:center">MALHAR_GREEENS , Kolkata</h6>
+</footer>
+<!-- Footer -->
+	</div>
+		<!-- <b-row>
 			
 			<b-col cols="9">
 				<b-row>
@@ -77,4 +237,4 @@ export default {
 	async created() {},
 	methods: {},
 };
-</script>
+</script> -->
